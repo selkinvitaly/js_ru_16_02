d75fbe809610c582e452bbb1980dfe4186ccff23
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-01 19:34:55 +0200

    add few comments

--- src/fixtures.js
+++ src/fixtures.js
@@ -4,11 +4,15 @@ export default [
         body: 'Lorem Ipsum',
         id: 1,
         comments: [
-            {text: 'hello', id: 1}
+            {text: 'hello', id: 1},
+            {text: 'other comment', id: 2}
         ]
     },{
         title: 'Other Article',
         body: 'Some other text',
+        comments: [
+            {text: 'some new comment', id: 3}
+        ],
         id: 2
     },{
         title: 'Other Article!!!!',
