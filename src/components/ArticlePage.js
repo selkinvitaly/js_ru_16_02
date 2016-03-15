95a1f7fa3be391bc2f045bc42f4d00d83d711a6d
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-15 21:24:20 +0200

    move retrieving logic

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -7,13 +7,12 @@ class ArticlePage extends Component {
     constructor(props) {
         super()
         const { params: { id }} = props
+        setTimeout(() => loadArticleById({id: id}), 0)
         this.state = {
             article: articlesStore.getById(id)
         }
     }
     componentDidMount() {
-        const { params: {id}} = this.props
-        setTimeout(() => loadArticleById({id}), 0)
         articlesStore.addChangeListener(this.change)
     }
 
@@ -22,6 +21,7 @@ class ArticlePage extends Component {
     }
 
     componentWillReceiveProps(props) {
+        setTimeout(() => loadArticleById({id: props.params.id}), 0)
         this.setState({
             article: articlesStore.getById(props.params.id)
         })
