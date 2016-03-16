936f0bb3677c1df3a7a855144df62d799b8dc7e2
Selkin Vitaly <contact@selkinvitaly.ru>
2016-03-17 01:00:22 +0300

    updated actions

--- src/actions/api/comment.js
+++ src/actions/api/comment.js
@@ -3,3 +3,9 @@ import $ from 'jquery'
 export function loadForArticle({ articleId }) {
     return $.get(`/api/comment?article=${articleId}`)
 }
+
+export function loadByPage({ page, limit }) {
+  const offset = (page - 1) * limit;
+
+  return $.get(`/api/comment?limit=${limit}&offset=${offset}`);
+}
