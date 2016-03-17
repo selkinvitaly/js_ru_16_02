858e1fec64008518946638fea39b22f2a8c6daaa
Selkin Vitaly <contact@selkinvitaly.ru>
2016-03-17 10:46:10 +0300

    updated MyCommentsStore

--- src/stores/MyCommentsStore.js
+++ src/stores/MyCommentsStore.js
@@ -1,34 +1,27 @@
 "use strict";
 
 import { EventEmitter } from "events";
-const CHANGE_EVENT = "CHANGE_EVENT";
+const CHANGE_EVENT = "COMMENTS_UPDATE";
 
 import { LOAD_COMMENTS_BY_PAGE, _SUCCESS, _START } from "../actions/constants";
 import dispatcher from "../dispatcher";
+import myModel from "./MyModel";
 import * as actions from "../actions/comment";
 
 class MyCommentsStore extends EventEmitter {
-  constructor(stores, initialState) {
+  constructor(stores) {
     super();
 
     this._stores = stores;
     this._items = {};
-    this._lastTimeStamp = 0;
-    this._interval = 30000;
     this._loading = false;
     this._total = null;
-
-    if (initialState) {
-      initialState.forEach(this.add.bind(this));
-    }
 
     dispatcher.register(action => {
       const { type, data, response } = action;
 
       switch (type) {
         case LOAD_COMMENTS_BY_PAGE + _SUCCESS:
-          console.log(response);
-
           let key = `${data.page}:${data.limit}`;
           let items = response.records;
 
@@ -56,7 +49,7 @@ class MyCommentsStore extends EventEmitter {
   }
 
   add(key, items) {
-    this._items[key] = items;
+    this._items[key] = myModel(items);
   }
 
   emitChange() {
@@ -72,16 +65,22 @@ class MyCommentsStore extends EventEmitter {
   }
 
   getByPage(key) {
-    const key = `${page}:${limit}`;
+    return this._items[key].sort((a, b) => a.id - b.id);
+  }
 
-    return this._items[key] ? this._items[key].slice().sort((a, b) => a.id - b.id) : [];
+  wasLoaded(key) {
+    return this._items[key] ? !!this._items[key].loaded : false;
   }
 
   getOrLoadByPage(page, limit = 10) {
-    if ((Date.now() - this._lastTimeStamp) > this._interval) {
+    const key = `${page}:${limit}`;
+
+    if (!this.wasLoaded(key)) {
+      console.log("FROM API"); // dev
       actions.loadCommentsByPage({page, limit});
     } else {
-      return this.getByPage(`${page}:${limit}`);
+      console.log("FROM STORE"); // dev
+      return this.getByPage(key);
     }
   }
 
