c8a04f6a694478cb51cab42d65f392b2c0ea8240
Selkin Vitaly <contact@selkinvitaly.ru>
2016-03-20 16:47:53 +0300

    added access checks for new article

--- src/components/NewArticle.js
+++ src/components/NewArticle.js
@@ -1,14 +1,32 @@
 import React, { Component, PropTypes } from 'react'
+import { login } from '../actions/user'
 
 class NewArticle extends Component {
-    static propTypes = {
 
-    };
+    static contextTypes = {
+        user: PropTypes.string
+    }
+
+    getContent() {
+        return <h2>New Article Page</h2>
+    }
+
+    loginRequired() {
+        return <div>
+            <p>You need login:</p>
+            <p><button onClick={this.login}>Login</button></p>
+        </div>
+    }
+
+    login = (ev) => {
+        ev.preventDefault()
+        login()
+    }
 
     render() {
         return (
             <div>
-                <h2>New Article Page</h2>
+                this.context.user ? {this.getContent()} : {this.loginRequired()}
             </div>
         )
     }
